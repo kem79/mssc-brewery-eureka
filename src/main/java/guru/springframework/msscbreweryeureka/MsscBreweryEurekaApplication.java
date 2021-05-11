c5779b2dafedd64455299ee9b4850f580330fd09
Marc <marc.marechal@dell.com>
2021-05-11 14:27:22 +0800

    Configure Service Discovery in Beer Service

--- src/main/java/guru/springframework/msscbreweryeureka/MsscBreweryEurekaApplication.java
+++ src/main/java/guru/springframework/msscbreweryeureka/MsscBreweryEurekaApplication.java
@@ -3,7 +3,9 @@ package guru.springframework.msscbreweryeureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Profile;
 
+@Profile("local-discovery")
 @EnableEurekaServer
 @SpringBootApplication
 public class MsscBreweryEurekaApplication {
